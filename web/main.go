4309bdf22b155cd60aa4ac7f89aaff27dffa6c32
Zahid Mujaddid <archazid@outlook.com>
2020-08-18 09:24:45 +0700

    Added initial go codes and assets

---
+++ web/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./web/ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
+}
